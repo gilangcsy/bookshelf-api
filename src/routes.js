e5c8f35b2a85ca1f818f317bbc4bf31af59f79e7
Gilang C. Syahputra <gilangchandra9@gmail.com>
2021-09-26 20:00:18 +0700

    inisiasi proyek, dan menambahkan fitur create dan read

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+const { addBookHandler, getBookHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getBookHandler,
+  },
+];
+
+module.exports = routes;
